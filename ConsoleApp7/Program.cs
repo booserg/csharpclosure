98e6b89e80da4b07b4b0869e41c9959db55a049d
Sergey Yevtushik <sergey@yevtushik.online>
2018-10-04 11:28:34 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Action> actions = new List<Action>();
+			for (int i = 0; i < 10; i++)
+				actions.Add(() => Console.WriteLine(i));
+
+			foreach (var act in actions)
+				act();
+		}
+	}
+}
